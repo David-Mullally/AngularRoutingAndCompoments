0a36f5d8104d216730cca92b8ecc74ff2072dbc9
Dave Mullally <mullallydavid7@gmail.com>
2023-08-25 14:26:43 +0200

    add example modules and basic routing

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,7 +1,10 @@
+import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'collections', component: CollectionsHomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/elements/elements-routing.module.ts
+++ src/app/elements/elements-routing.module.ts
@@ -1,7 +1,10 @@
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'elements', component: ElementsHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
