de84b021c6fa8d1b29394e5f0b8e38b60ac64fee
Dave Mullally <mullallydavid7@gmail.com>
2023-08-25 15:38:22 +0200

    implement somet lazy loading

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'collections', component: CollectionsHomeComponent }
+  {path: '', component: CollectionsHomeComponent }
 ];
 
 @NgModule({
--- src/app/elements/elements-routing.module.ts
+++ src/app/elements/elements-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'elements', component: ElementsHomeComponent}
+  {path: '', component: ElementsHomeComponent}
 ];
 
 @NgModule({
