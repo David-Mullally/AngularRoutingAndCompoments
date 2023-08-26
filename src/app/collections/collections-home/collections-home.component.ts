c2e009307f501c1031594c4567154dc3b6ef0e60
Dave Mullally <mullallydavid7@gmail.com>
2023-08-26 09:59:05 +0200

    add a customizable table that displays dynamic data

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent {
-
+  data:any = [
+    { name: 'Dengo', age: 40, job: 'Full Stack Web Developer', employed: true },
+    { name: 'Doodles', age: 30, job: 'Software Tester', employed: true},
+    { name: 'Aoife', age: 1, job: 'Baby', employed: false},
+  ];
+  headers: any = [
+    { key: 'employed', label: 'Employed' },
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
 }
