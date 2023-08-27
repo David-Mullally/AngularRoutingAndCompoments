a621f4e933854827bb8298bf7f162088d7ced5e9
Dave Mullally <mullallydavid7@gmail.com>
2023-08-27 16:08:51 +0200

    finish views page

--- src/app/views/statistics/statistics.component.ts
+++ src/app/views/statistics/statistics.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.css']
 })
-export class StatisticsComponent {
-
+export class StatisticsComponent implements OnInit {
+  @Input() data:any = [];
+  constructor() { }
+  ngOnInit(){}
 }
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsComponent } from '../views/views.component';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 
 @NgModule({
   declarations: [
     ViewsComponent,
     ViewsHomeComponent,
-    StatisticsComponent
+    StatisticsComponent,
+    ItemListComponent
   ],
   imports: [
     CommonModule,
     ViewsRoutingModule,
+    SharedModule
   ]
 })
 export class ViewsModule { }
